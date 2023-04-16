6639cbfe31bf673b3ef83dcc6e2fe0d080b4a538
Lango <whl2644992002@gmail.com>
2023-04-16 10:02:56 +0800

    master commit5

--- src/main/java/com/qingwa/usercenter/common/ErrorCode.java
+++ src/main/java/com/qingwa/usercenter/common/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     PARAMS_ERROR(40000,"请求参数错误",""),
     NULL_ERROR(40001,"请求数据为空",""),
     NOT_LOGIN(40100,"用户未登录",""),
-    NO_AUTH(40101,"无用户权限",""),
+    NO_AUTH(40101,"无权限",""),
     SYSTEM_ERROR(50000,"系统内部异常","");
 
     private final int code;
