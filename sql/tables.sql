1a9fa1bd3c8fd213be0d5116a7c117c8b6bcedcd
Lango <whl2644992002@gmail.com>
2023-05-05 11:03:15 +0800

    end

--- sql/tables.sql
+++ sql/tables.sql
@@ -19,3 +19,26 @@ create table user
 )
     comment '用户';
 
+alter table user add COLUMN tags varchar(1024) null comment '标签列表';
+
+
+-- auto-generated definition
+create table tag
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    tagName    varchar(256)                       null comment '标签名称',
+    userId     bigint                             null comment '用户 Id',
+    parentId   bigint                             null comment '父标签 Id',
+    isParent   tinyint                            null comment '0 - 不是父标签、1 - 父标签',
+    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '修改时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除',
+    constraint uniIdx_tagName
+        unique (tagName)
+)
+    comment '标签';
+
+create index idx_userId
+    on tag (userId);
+
